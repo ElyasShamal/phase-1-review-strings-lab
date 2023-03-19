8362d359c48aa817e9d641a196957aa2340d34bf
Elyas <Elyas.shamal2@gmail.com>
2023-03-18 21:16:19 -0400

    review-string-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = "Elyas Shamal";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+// console.log(shortGreeting);
+// result =  Welcome, E!
